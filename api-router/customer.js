bbaa2b33f2d75d7d13202046f7b1c221300ae716
ge73led <melih.oezbeyli@tum.de>
2019-07-09 23:47:41 +0200

    Add customer update and remove fields

--- api-router/customer.js
+++ api-router/customer.js
@@ -1,19 +1,26 @@
-const { Router } = require('express');
-const { customer, item, comment} = require('../controllers');
-const { addRoute, addCustomerRoute, methods } = require('./util');
+const { Router } = require("express");
+const { customer, item, comment } = require("../controllers");
+const { addRoute, addCustomerRoute, methods } = require("./util");
 
 const router = Router();
 // Account
-addRoute(router, methods.POST, '/login', customer.login);
-addRoute(router, methods.POST, '/register', customer.register);
-addCustomerRoute(router, methods.GET, '/info', customer.info);
-addCustomerRoute(router, methods.PUT, '/password', customer.changePassword);
+addRoute(router, methods.POST, "/login", customer.login);
+addRoute(router, methods.POST, "/register", customer.register);
+addCustomerRoute(router, methods.GET, "/info", customer.info);
+addCustomerRoute(router, methods.PUT, "/password", customer.changePassword);
+addCustomerRoute(router, methods.PUT, "/info", customer.update);
+addCustomerRoute(router, methods.DELETE, "/info", customer.remove);
 // Inventory
-addRoute(router, methods.GET, '/inventory', item.getInventory);
-addRoute(router, methods.GET, '/item', item.getItem);
-addCustomerRoute(router, methods.POST, '/comment/create', comment.create);
-addCustomerRoute(router, methods.GET, '/comments/getAll', comment.getAll);
-addCustomerRoute(router, methods.PUT, '/comment/update', comment.update);
-addCustomerRoute(router, methods.DELETE, '/comment/deleteComment', comment.deleteComment);
+addRoute(router, methods.GET, "/inventory", item.getInventory);
+addRoute(router, methods.GET, "/item", item.getItem);
+addCustomerRoute(router, methods.POST, "/comment/create", comment.create);
+addCustomerRoute(router, methods.GET, "/comments/getAll", comment.getAll);
+addCustomerRoute(router, methods.PUT, "/comment/update", comment.update);
+addCustomerRoute(
+  router,
+  methods.DELETE,
+  "/comment/deleteComment",
+  comment.deleteComment
+);
 
 module.exports = router;
