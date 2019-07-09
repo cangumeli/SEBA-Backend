c1c1e1eaccb2ac72e38b2e471c3517184bff6dfe
aybarburak <aybarburak@gmail.com>
2019-07-09 20:23:22 +0200

    Add routes

--- api-router/customer.js
+++ api-router/customer.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { customer, item } = require('../controllers');
+const { customer, item, comment} = require('../controllers');
 const { addRoute, addCustomerRoute, methods } = require('./util');
 
 const router = Router();
@@ -8,5 +8,9 @@ addRoute(router, methods.POST, '/register', customer.register);
 addCustomerRoute(router, methods.GET, '/info', customer.info);
 // Inventory
 addRoute(router, methods.GET, '/inventory', item.getInventory);
+addCustomerRoute(router, methods.POST, '/comment/create', comment.create);
+addCustomerRoute(router, methods.GET, '/comments/getAll', comment.getAll);
+addCustomerRoute(router, methods.PUT, '/comment/update', comment.update);
+addCustomerRoute(router, methods.DELETE, '/comment/deleteComment', comment.deleteComment);
 
 module.exports = router;
--- api-router/owner.js
+++ api-router/owner.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { methods, addRoute, addOwnerRoute } = require('./util');
-const { owner, shop, item } = require('../controllers');
+const { owner, shop, item, comment } = require('../controllers');
 
 const router = Router();
 // Account operations
@@ -11,6 +11,7 @@ addOwnerRoute(router, methods.GET, '/info', owner.info);
 addOwnerRoute(router, methods.POST, '/shop', shop.createShop);
 addOwnerRoute(router, methods.PUT, '/shop', shop.updateShop);
 addOwnerRoute(router, methods.GET, '/shops', shop.getShops);
+addOwnerRoute(router, methods.GET, '/comments/getAll', comment.getAll);
 // Inventory operations
 addOwnerRoute(router, methods.POST, '/inventory', item.addInventory);
 addRoute(router, methods.GET, '/inventory', item.getInventory);
