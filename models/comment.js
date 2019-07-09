f838a8510c8cb74efb6b4fb793409b62f905e2b0
aybarburak <aybarburak@gmail.com>
2019-07-10 00:09:08 +0200

    Fix format for comments

--- models/comment.js
+++ models/comment.js
@@ -1,28 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
-    text: {type: String, required: true},
-    rating: {type: Number, min: 1, max:5, required: true},
-    date: {type: Date},
-    upvote: {type: Number, default: 0},
-    downvote: {type: Number, default: 0},
-    userId: {type: mongoose.SchemaTypes.ObjectId, required: true}
-})
+  text: { type: String, required: true },
+  rating: { type: Number, min: 1, max: 5, required: true },
+  date: { type: Date },
+  upvote: { type: Number, default: 0 },
+  downvote: { type: Number, default: 0 },
+  userId: { type: mongoose.SchemaTypes.ObjectId, required: true }
+});
 
-const Comment = mongoose.model('Comment', commentSchema);
+const Comment = mongoose.model("Comment", commentSchema);
 
 module.exports.Comment = Comment;
 
 module.exports.ShopComment = Comment.discriminator(
-    'ShopComment',
-    new mongoose.Schema({
-        shopId: {type: mongoose.SchemaTypes.ObjectId, index: true}
-    })
+  "ShopComment",
+  new mongoose.Schema({
+    shopId: { type: mongoose.SchemaTypes.ObjectId, index: true }
+  })
 );
 
 module.exports.ItemComment = Comment.discriminator(
-    'ItemComment',
-    new mongoose.Schema({
-        itemId: { type: mongoose.SchemaTypes.ObjectId, index: true}
-    })
+  "ItemComment",
+  new mongoose.Schema({
+    itemId: { type: mongoose.SchemaTypes.ObjectId, index: true }
+  })
 );
