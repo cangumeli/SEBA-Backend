0e7008c9676ad0a28e46d270a636c842fb404a0b
Can Guemeli <can.guemeli@tum.de>
2019-07-09 23:17:26 +0200

    Upvote/downvote

--- api-router/customer.js
+++ api-router/customer.js
@@ -11,9 +11,12 @@ addCustomerRoute(router, methods.PUT, '/password', customer.changePassword);
 // Inventory
 addRoute(router, methods.GET, '/inventory', item.getInventory);
 addRoute(router, methods.GET, '/item', item.getItem);
+// Comment
 addCustomerRoute(router, methods.POST, '/comment/create', comment.create);
 addCustomerRoute(router, methods.GET, '/comments/getAll', comment.getAll);
 addCustomerRoute(router, methods.PUT, '/comment/update', comment.update);
 addCustomerRoute(router, methods.DELETE, '/comment/deleteComment', comment.deleteComment);
+addCustomerRoute(router, methods.PUT, '/comment/upvote', comment.upvote);
+addCustomerRoute(router, methods.PUT, '/comment/downvote', comment.downvote);
 
 module.exports = router;
