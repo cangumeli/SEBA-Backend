51f47c385dfba56cb7240ff58ef5ff90bb5e4e74
Elif Erbil <eerbil13@ku.edu.tr>
2019-07-13 13:30:45 +0200

    add shopping list controller

--- api-router/customer.js
+++ api-router/customer.js
@@ -1,37 +1,35 @@
-const { Router } = require("express");
-const { customer, item, comment, shoppingList } = require("../controllers");
-const {
-  addRoute,
-  addCustomerRoute,
-  methods,
-  addStaticRoute
-} = require("./util");
-const { file: fileService } = require("../services");
+const { Router } = require('express');
+const { customer, item, comment, shoppingList } = require('../controllers');
+const { addRoute, addCustomerRoute, methods, addStaticRoute } = require('./util');
+const { file: fileService } = require('../services');
 
 const router = Router();
 // Account
-addRoute(router, methods.POST, "/login", customer.login);
-addRoute(router, methods.POST, "/register", customer.register);
-addCustomerRoute(router, methods.GET, "/info", customer.info);
-addCustomerRoute(router, methods.PUT, "/password", customer.changePassword);
-addCustomerRoute(router, methods.PUT, "/info", customer.update);
-addCustomerRoute(router, methods.DELETE, "/info", customer.remove);
-addCustomerRoute(router, methods.POST, "/image", customer.uploadPic);
-addStaticRoute(router, "/image", fileService.dirs.CUSTOMER_PROFILE_PICTURES);
-addCustomerRoute(router, methods.DELETE, "/image", customer.removePicture);
+addRoute(router, methods.POST, '/login', customer.login);
+addRoute(router, methods.POST, '/register', customer.register);
+addCustomerRoute(router, methods.GET, '/info', customer.info);
+addCustomerRoute(router, methods.PUT, '/password', customer.changePassword);
+addCustomerRoute(router, methods.PUT, '/info', customer.update);
+addCustomerRoute(router, methods.DELETE, '/info', customer.remove);
+addCustomerRoute(router, methods.POST, '/image', customer.uploadPic);
+addStaticRoute(router, '/image', fileService.dirs.CUSTOMER_PROFILE_PICTURES);
+addCustomerRoute(router, methods.DELETE, '/image', customer.removePicture);
 // Inventory
-addRoute(router, methods.GET, "/inventory", item.getInventory);
-addRoute(router, methods.GET, "/item", item.getItem);
+addRoute(router, methods.GET, '/inventory', item.getInventory);
+addRoute(router, methods.GET, '/item', item.getItem);
 // Comment
-addCustomerRoute(router, methods.GET, "/comment", comment.get);
-addCustomerRoute(router, methods.POST, "/comment", comment.create);
-addCustomerRoute(router, methods.PUT, "/comment", comment.update);
-addCustomerRoute(router, methods.GET, "/comments", comment.getAll);
-addCustomerRoute(router, methods.DELETE, "/comment", comment.deleteComment);
-addCustomerRoute(router, methods.PUT, "/comment/upvote", comment.upvote);
-addCustomerRoute(router, methods.PUT, "/comment/downvote", comment.downvote);
+addRoute(router, methods.GET, '/comments', comment.getAll);
+addCustomerRoute(router, methods.GET, '/comment', comment.get);
+addCustomerRoute(router, methods.POST, '/comment', comment.create);
+addCustomerRoute(router, methods.PUT, '/comment', comment.update);
+addCustomerRoute(router, methods.DELETE, '/comment', comment.deleteComment);
+addCustomerRoute(router, methods.PUT, '/comment/upvote', comment.upvote);
+addCustomerRoute(router, methods.PUT, '/comment/downvote', comment.downvote);
 // Shopping list
-addCustomerRoute(router, methods.POST, "/shoppingList", shoppingList.addItem);
+addCustomerRoute(router, methods.GET, '/shoppingList', shoppingList.get);
+addCustomerRoute(router, methods.POST, '/shoppingList', shoppingList.addItem);
+addCustomerRoute(router, methods.DELETE, '/shoppingListItem', shoppingList.removeItem);
+addCustomerRoute(router, methods.DELETE, '/shoppingList', shoppingList.removeAll);
 // Shop
-addStaticRoute(router, "/shop/image", fileService.dirs.SHOP_PICTURES);
+addStaticRoute(router, '/shop/image', fileService.dirs.SHOP_PICTURES);
 module.exports = router;
--- controllers/shopping-list.js
+++ controllers/shopping-list.js
@@ -1,21 +1,63 @@
+const { api: apiService } = require('../services');
 const { ShoppingList } = require('../models');
 
 const addItem = {
-    validation: {
-        fields: [
-            {name: 'itemId', required: true, type: 'string'}
-        ]
-    },
-    async endpoint({body, payload}) {
-        let list = await ShoppingList.findOne({user: payload.id});
-        if (!list) {
-            list = new ShoppingList({user: payload.id});
-        }
-        list.items.addToSet(body.itemId);
-        return await list.save();
+  validation: {
+    fields: [{ name: 'itemId', required: true, type: 'string' }],
+  },
+  async endpoint({ body, payload }) {
+    let list = await ShoppingList.findOne({ user: payload.id });
+    if (!list) {
+      list = new ShoppingList({ user: payload.id });
     }
+    list.items.addToSet(body.itemId);
+    return list.save();
+  },
+};
+
+const get = {
+  validation: {
+    fields: [],
+  },
+  async endpoint({ payload }) {
+    let list = await ShoppingList.findOne({ user: payload.id });
+    if (!list) {
+      list = new ShoppingList({ user: payload.id });
+    }
+    return list.items;
+  },
+};
+
+const removeItem = {
+  validation: {
+    fields: [{ name: 'itemId', required: true, type: 'string' }],
+  },
+  async endpoint({ body, payload }) {
+    const list = await ShoppingList.findOne({ user: payload.id });
+    apiService.errorIf(!list, apiService.errors.NOT_FOUND, 'ShoppingListDoesntExist');
+    apiService.errorIf(
+      !list.items.includes(body.itemId),
+      apiService.errors.NOT_FOUND,
+      'ItemNotInShoppingList',
+    );
+    list.items.pull({ _id: body.itemId });
+    return list.save();
+  },
+};
+
+const removeAll = {
+  validation: { fields: [] },
+  async endpoint({ payload }) {
+    const list = await ShoppingList.findOne({ user: payload.id });
+    apiService.errorIf(!list, apiService.errors.NOT_FOUND, 'ShoppingListDoesntExist');
+    list.items = [];
+    return list.save();
+  },
 };
 
 module.exports = {
-    addItem
+  addItem,
+  get,
+  removeItem,
+  removeAll,
 };
