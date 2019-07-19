821a8dc4d3a5e8025d55e58e44009b426d9c0e6c
Elif Erbil <eerbil13@ku.edu.tr>
2019-07-19 15:09:06 +0200

    change item model/controller for multiple tags-categories

--- models/item.js
+++ models/item.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const itemSchema = new mongoose.Schema({
   shopId: { type: mongoose.SchemaTypes.ObjectId, required: true, index: true, ref: 'Shop' },
   name: { type: String, required: true },
-  category: { type: String, required: true, index: true },
-  tag: { type: String, required: true },
+  category: { type: [String], required: true },
+  tag: { type: [String], required: true },
   price: { type: Number, required: true },
   material: { type: String },
   isSponsored: { type: Boolean },
